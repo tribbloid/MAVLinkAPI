43610aba22e1c796369c554fef4b4cd1c8b2fffe
Peng Cheng <peng.cheng@acyclic.ai>
2025-06-07 20:42:19 -0400

    fix asmdef namespace of TableLayout

--- __module~/generator/dev/ArduPilot/compose.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC2034
-elements=(
-  "ardupilotmega"
-  "offspec"
-)
--- __module~/generator/dev/HPVDT_Falcon/compose.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC2034
-elements=(
-  "minimal"
-  "hpvdt/falcon_sensors"
-)
